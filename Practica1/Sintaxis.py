e4e755f321a2879993c7be7ba4f547e4987d6d10
3d1th-u <uribeordonezedith@gmail.com>
2025-05-16 18:19:15 -0600

    Variables con Python

--- Practica1/Sintaxis.py
+++ Practica1/Sintaxis.py
@@ -7,5 +7,28 @@ varias lineas en Python
 """
 
 #2. strings 
-print("hola soy una cacena")
 print('hola soy otra cacena')
+
+variable1= "hola soy una cadena"
+print(len(variable1))
+
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. variables
+
+x= "Edith Uribe"
+x= 4
+x= 5.74
+
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
