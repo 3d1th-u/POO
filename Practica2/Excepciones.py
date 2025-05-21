9ea98c8fca45bec80cf7e5fd2066036914b9a843
3d1th-u <uribeordonezedith@gmail.com>
2025-05-20 18:00:24 -0600

    ManejoDeExcepciones

---
+++ Practica2/Excepciones.py
@@ -0,0 +1,11 @@
+
+try:
+    numero = int(input("Introduce un numero: "))
+    resultado = 10 / numero
+    
+    print("Resultado: ", resultado)
+    
+except ValueError:
+    print("Error: Se ingreso algo que no es un numero entero.")
+except ZeroDivisionError:
+    print("Error: Estas intentado dividir entre 0.")
