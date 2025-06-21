b126cec8aafb703e214ac8d6f09c53e5eed0f0cb
3d1th-u <uribeordonezedith@gmail.com>
2025-06-20 18:50:41 -0600

    errores
    agregamos algunos manejos de errores para los input.

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask,jsonify
+from flask import Flask ,jsonify,render_template,request, url_for,flash,redirect
 from flask_mysqldb import MySQL
 import MySQLdb
 
@@ -13,9 +13,67 @@ app.config['MYSQL_USER']="root"
 app.config['MYSQL_PASSWORD']="3d1th"
 app.config['MYSQL_DB']="dbflask"
 #usar solo si hay un cambio de puerto
-app.config['MYSQL_PORT']=3306 
+#app.config['MYSQL_PORT']=3306 
+app.secret_key='mysecretkey'
 
 mysql=MySQL(app)
+
+
+#ruta de inicio
+@app.route('/')
+def home():
+    return render_template('formulario.html')
+
+#ruta de consulta
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
+
+# ruta para el insert
+@app.route('/guardarAlbum',methods=['POST'])
+def guardar():
+    
+    #listas de errores
+    errores={}
+    
+    #obtener los datos a insertar
+    #inputs de la vista
+    titulo= request.form.get('txtTitulo','').strip()
+    artista= request.form.get('txtArtista','').strip()
+    anio= request.form.get('txtAnio','').strip()
+    
+    
+    if not titulo:
+        errores['txtTitulo']='Nombre del album OBLIGATORIO'
+    if not artista:
+        errores['txtArtista']='Nombre del artista OBLIGATORIO'
+    if not anio:
+        errores['txtAnio']='Año es OBLIGATORIO'
+    elif not anio.isdigit() or int(anio)< 1800 or int(anio)> 2100:
+        errores['txtAnio']='Ingresa un año valido'
+        
+    
+    
+    if not errores:
+    #intentamos ejecutar el insert
+        try:
+            cursor= mysql.connection.cursor()
+            cursor.execute('insert into tb_albums(albums,artista,anio) values(%s,%s,%s)',(titulo,artista,anio))
+            mysql.connection.commit()
+            flash('El album se guardo en la BD')
+            return redirect(url_for('home'))
+        
+        except:
+            mysql.connection.rollback()
+            flash('Algo fallo: '+ str(e))
+            return redirect(url_for('home'))
+        
+        finally:
+            cursor.close()
+    
+    return render_template('formulario.html',errores=errores)
+#return render_template('consulta.html')
+
 
 # ruta para probar la conexion con mysql
 @app.route('/DBCheck')
@@ -28,14 +86,6 @@ def DB_ckeck():
         return jsonify({'status':'error','message':str(e)}),500
 
 
-@app.route('/')
-def home():
-    return 'Hola Mundo FLASK'
-
-#ruta con parametros
-@app.route('/saludo/<nombre>')
-def saludar(nombre):
-    return 'Hola,'+nombre+'!'
 
 #ruta try-catch
 #esta rura maneja los errores
@@ -48,17 +98,6 @@ def paginaNoE(e):
 def metodonoP(e):
     return 'revisa el metodo de envio de tu ruta (GET o POST) !',405
 
-
-#ruta doble
-@app.route('/usuario')
-@app.route('/usuaria')
-def dobleroute():
-    return 'soy el mismo recurso del servidor'
-
-#ruta POSt
-@app.route('/formulario',methods=['POST'])
-def formulario():
-    return 'Soy un formulario'
 
 
 
